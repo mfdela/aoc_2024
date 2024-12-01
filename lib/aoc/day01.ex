f80e990ce7798536dfb50688840a7f2adecddd88
Marco Delaurenti <mfd@fastmail.com>
2024-12-01 18:07:43 +0000

    Reduce from O(n^2) to O(n)

--- lib/aoc/day01.ex
+++ lib/aoc/day01.ex
@@ -12,11 +12,12 @@ defmodule Aoc.Day01 do
     |> Enum.map(&String.split(&1, ~r/\s+/))
     |> Enum.map(fn [x, y] -> {String.to_integer(x), String.to_integer(y)} end)
     |> Enum.unzip()
+    |> Tuple.to_list()
   end
 
-  def sort({list1, list2}), do: {Enum.sort(list1), Enum.sort(list2)}
+  def sort([list1, list2]), do: [Enum.sort(list1), Enum.sort(list2)]
 
-  def list_difference({list1, list2}) do
+  def list_difference([list1, list2]) do
     Enum.zip(list1, list2)
     |> Enum.reduce(0, fn {a, b}, s -> s + abs(a - b) end)
   end
@@ -24,8 +25,9 @@ defmodule Aoc.Day01 do
   def part2(args) do
     args
     |> clean_input()
-    |> then(fn {list1, list2} ->
-      Enum.reduce(list1, 0, fn x, acc -> acc + x * Enum.count(list2, &(&1 == x)) end)
+    |> Enum.map(&Enum.frequencies/1)
+    |> then(fn [map1, map2] ->
+      Enum.reduce(Map.keys(map1), 0, fn x, acc -> acc + x * map1[x] * Map.get(map2, x, 0) end)
     end)
   end
 end
